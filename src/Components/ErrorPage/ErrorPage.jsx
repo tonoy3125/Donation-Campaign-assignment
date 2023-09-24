d60c61a75a65911c4e756a960aac96d7a230cc18
tonoy3125 <shaifshajedt@gmail.com>
2023-09-24 20:38:20 +0600

    errorpage readded commit

--- src/Components/ErrorPage/ErrorPage.jsx
+++ src/Components/ErrorPage/ErrorPage.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 
 
 const ErrorPage = () => {
@@ -5,7 +6,8 @@ const ErrorPage = () => {
         <div className="text-center mt-40">
             <h1 className="text-2xl lg:text-4xl font-bold">Opsss!!!!!!!</h1>
             <h1 className="text-2xl lg:text-4xl mt-3 font-bold">Unexpected Application Error!</h1>
-            <p className="text-base mt-3 font-medium">404 Not Found</p>
+            <p className="text-base mt-3 font-medium mb-3">404 Not Found</p>
+            <Link to="/"><button className="btn btn-accent text-white">Go Home</button></Link>
         </div>
     );
 };
